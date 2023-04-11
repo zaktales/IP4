70255f81dab0e3d55523af9ca812c57a8d680e14
zaktales <wizz@pop-os.localdomain>
2023-04-11 13:30:03 +0300

    modified k8s manifest files

---
+++ client/build/precache-manifest.d34f9ac5bb5f5b3b360240f1b62d2440.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401efd4d131426c4d7bf30bff8f36740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7ea6c0357612804a6a6",
+    "url": "/static/css/main.430fb779.chunk.css"
+  },
+  {
+    "revision": "d02cc5346a03d410f78f",
+    "url": "/static/js/2.0b7d32d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b7d32d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ea6c0357612804a6a6",
+    "url": "/static/js/main.189cf401.chunk.js"
+  },
+  {
+    "revision": "004ed4c4fb34dd3cf9e0",
+    "url": "/static/js/runtime-main.f195edbc.js"
+  },
+  {
+    "revision": "f1ec4643c0cd6bbd810bcbfb49e0d0aa",
+    "url": "/static/media/about.f1ec4643.jpg"
+  },
+  {
+    "revision": "45c8d458a08624f77891e0554a9288b4",
+    "url": "/static/media/facebook.45c8d458.png"
+  },
+  {
+    "revision": "44685054b1ee08ca34d9246e9cc304f9",
+    "url": "/static/media/flickr.44685054.png"
+  },
+  {
+    "revision": "4a710ee86f36d3d3955359bbcba5f8cf",
+    "url": "/static/media/g_plus.4a710ee8.png"
+  },
+  {
+    "revision": "170664a66f3ee0327630531403d709fe",
+    "url": "/static/media/hero.170664a6.jpg"
+  },
+  {
+    "revision": "1e2c252255afe8fca1c41c874b22ac75",
+    "url": "/static/media/logo.1e2c2522.png"
+  },
+  {
+    "revision": "f90edb1571f4d73e4e6552c0d02a42c7",
+    "url": "/static/media/pinterest.f90edb15.png"
+  },
+  {
+    "revision": "d79052ba122439f0ced0e53f7e826b33",
+    "url": "/static/media/shop.d79052ba.png"
+  },
+  {
+    "revision": "f456e05870777161185fa50fa86fd0f1",
+    "url": "/static/media/skype.f456e058.png"
+  },
+  {
+    "revision": "4d2a6f5ed6eff26d5372e130e910c25d",
+    "url": "/static/media/stumble_upon.4d2a6f5e.png"
+  },
+  {
+    "revision": "af34349ade6fe99ceaf060718e23626c",
+    "url": "/static/media/twitter.af34349a.png"
+  },
+  {
+    "revision": "a55e26f73cf812655119636422345b32",
+    "url": "/static/media/you_tube.a55e26f7.png"
+  }
+]);
